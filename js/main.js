f389480ca21b827ccdcbbd4047cadeb3221aa6df
Patrick Best <patrickbest@patrickbest.com>
2015-04-02 17:26:35 -0600

    init boilerplace

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* jshint browser: true, devel: true, indent: 2 */
+/* global $, jQuery, document */
+
+function l(data) {
+  'use strict';
+  console.log(l);
+}
+
+jQuery(document).ready(function () {
+  'use strict';
+  console.log('Hola Globie');
+});
