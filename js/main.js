bf0180ffd8def743d665355dbd3b7857d1efe424
mikey <michaelrayvon@gmail.com>
2015-04-08 12:58:03 -0500

    adds debounce, image resize for slider

--- js/main.js
+++ js/main.js
@@ -10,6 +10,22 @@ jQuery(document).ready(function () {
   'use strict';
   console.log('Hola Globie');
 
+// RESIZE
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  };
+
 // PACKERY
   if ( $('.js-packery-container').length ) {
     $('.js-packery-container').imagesLoaded( function() {
@@ -25,13 +41,34 @@ jQuery(document).ready(function () {
   }
 
 // SLICK
+  function resizeImages() {
+    var $images = $('.js-slick-item img');
+    var windowHeight = $(window).height();
+    var margin = 70;
+    var captionHeight = -1;
+
+    $('.gallery-caption').each(function() {
+      captionHeight = captionHeight > $(this).height() ? captionHeight : $(this).height();
+    });
+
+    $images.css( 'height' , (windowHeight - margin - captionHeight) + 'px' );
+
+    $('.js-slick-container').css( 'padding-top' , ( margin / 2 ) );
+  }
+
+  var resizeFunction = debounce(function() {
+    resizeImages();
+  }, 50);
+
   function slickInit() {
-    $('.js-slick-container').slick({
-    }).css({
+    $('.js-slick-container').slick()
+    .css({
       'opacity': 1
     });
   }
+
   if ( $('.js-slick-item').length ) {
+    resizeImages();
     slickInit();
   }
 
