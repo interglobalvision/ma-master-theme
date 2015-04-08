977c246fd3c88dffc67369575b55a01d06dc3e15
Patrick Best <patrickbest@patrickbest.com>
2015-04-08 12:01:48 -0500

    improve 404

--- 404.php
+++ 404.php
@@ -4,9 +4,12 @@ get_header();
 
 <!-- main content -->
 
-<section id="main-content">
-  <h1>Not found</h1>
-  <p>:(</p>
+<section id="main-content" class="container">
+  <div class="row">
+    <div class="col col6">
+      <h2>Not found</h2>
+      <p>:(</p>
+    </div>
 <!-- end main-content -->
 </section>
 
