bf4fdbeecffd7f386505e26d625978a156da585d
mikey <michaelrayvon@gmail.com>
2015-04-08 12:59:26 -0500

    begins gallery metabox

--- single.php
+++ single.php
@@ -2,22 +2,17 @@
 get_header();
 ?>
 
-<!-- main content -->
-
-<main id="main-content" class="container">
-
-  <!-- main posts loop -->
-  <section id="posts" class="row">
-
 <?php
 if( have_posts() ) {
   while( have_posts() ) {
     the_post();
+    //$get_gallery = get_post_meta( $post->ID, '_igv_gallery', true );
+    //$gallery = do_shortcode( $get_gallery );
 ?>
 
-    <article <?php post_class('col col12'); ?> id="post-<?php the_ID(); ?>">
+    <article <?php post_class('viewer'); ?> id="post-<?php the_ID(); ?>">
 
-      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+      <?php // gallery ?>
 
 	    <?php the_content(); ?>
 
@@ -30,13 +25,6 @@ if( have_posts() ) {
     <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
 <?php
 } ?>
-
-  <!-- end posts -->
-  </section>
-
-<!-- end main-content -->
-
-</main>
 
 <?php
 get_footer();
