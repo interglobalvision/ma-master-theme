78ad7b2129c4937fca6ea3c38d5f919aa61ec20d
Patrick Best <patrickbest@patrickbest.com>
2015-04-08 20:15:14 -0500

    single.php formatting

--- single.php
+++ single.php
@@ -11,16 +11,33 @@ if( have_posts() ) {
 
     <article <?php post_class('viewer'); ?> id="post-<?php the_ID(); ?>">
 
+      <nav id="single-close" class="single-nav">
+        <a href="<?php echo home_url(); ?>">&#10005;</a>
+      </nav>
+      <nav id="single-next" class="single-nav">
+        <a href="<?php echo home_url(); ?>">&rarr;</a>
+      </nav>
+      <nav id="single-prev" class="single-nav">
+        <a href="<?php echo home_url(); ?>">&larr;</a>
+      </nav>
+
       <div class="js-slick-container slider">
         <?php echo do_shortcode($gallery) ?>
       </div>
       <div class="slider-text">
         <?php the_title(); ?>
         <span class="caption font-italic"></span>
+        <span id="slick-current-index">1</span> of <span id="slick-length"></span>
         <span class="arrow-prev"></span> | <span class="arrow-next"></span>
       </div>
 
-	    <?php the_content(); ?>
+      <section id="single-copy" class="container">
+        <div class="row">
+          <div class="col col6">
+	          <?php the_content(); ?>
+          </div>
+        </div>
+      </section>
 
     </article>
 
