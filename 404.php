7dc36cc1234e9562a70a8dc951e5abdac786dffb
Patrick Best <patrickbest@patrickbest.com>
2015-04-21 13:28:44 -0500

    more appropriate 404 copy lol

--- 404.php
+++ 404.php
@@ -7,8 +7,7 @@ get_header();
 <section id="main-content" class="container">
   <div class="row">
     <div class="col col6">
-      <h2>Not found</h2>
-      <p>:(</p>
+      <h2>404 Not found</h2>
     </div>
 <!-- end main-content -->
 </section>
